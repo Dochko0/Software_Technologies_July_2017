e895a5887c67c0b4fb6e019ca7b9d29bb6977d62
Dochko0 <dochko_dochev@yahoo.com>
2018-02-22 16:23:27 +0200

    Part Of Course

---
+++ EXAM/Skeletons/Java Solution/src/main/java/shoppinglist/bindingModel/ProductBindingModel.java
@@ -0,0 +1,49 @@
+package shoppinglist.bindingModel;
+
+import javax.persistence.criteria.CriteriaBuilder;
+
+public class ProductBindingModel {
+    private Integer priority;
+
+    private String name;
+
+    private Integer quantity;
+
+    private String status;
+
+    public ProductBindingModel() {
+    }
+
+
+    public Integer getPriority() {
+        return this.priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ WebCSharp/Bookibrary/Bookibrary/Controllers/BookController.cs
@@ -0,0 +1,185 @@
+﻿using Bookibrary.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bookibrary.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var books = db.Books.Include(b => b.Author).ToList();
+
+                return View(books);
+            }
+
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var book = db.Books.Include(b=>b.Author).SingleOrDefault(b=> b.Id == id);
+
+                if (book == null)
+                {
+                    return new HttpNotFoundResult($"Cannot find book with ID {id}");
+                }
+            return View(book);
+
+            }
+
+                
+        }
+
+        // GET: Book/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Book book)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var userId = User.Identity.GetUserId();
+
+                book.AuthorId = userId;
+
+                db.Books.Add(book);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var book = db.Books.SingleOrDefault(b => b.Id == id);
+
+                if (book == null)
+                {
+                    return new HttpNotFoundResult($"Cannot find book with ID {id}");
+                }
+
+                var userID = User.Identity.GetUserId();
+                if (book.AuthorId != userID)
+                {
+                    return new HttpUnauthorizedResult("You cannot touch this!");
+                }
+
+                return View(book);
+            }
+
+        }
+    
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Book bookViewModel)
+        {
+            if (bookViewModel.Title==null || bookViewModel.Description==null)
+            {
+                return View(bookViewModel);
+            }
+
+            using (var db = new ApplicationDbContext())
+            {
+                var book = db.Books.SingleOrDefault(b => b.Id == id);
+
+                if (book == null)
+                {
+                    return new HttpNotFoundResult($"Cannot find book with ID {id}");
+                }
+
+                var userID = User.Identity.GetUserId();
+                if (book.AuthorId != userID)
+                {
+                    return new HttpUnauthorizedResult("You cannot touch this!");
+                }
+
+
+                book.Title = bookViewModel.Title;
+                book.Description = bookViewModel.Description;
+                db.SaveChanges();
+
+            }
+
+
+            return RedirectToAction("Details", new { id = id });
+        }
+
+        // GET: Book/Delete/5
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var book = db.Books.SingleOrDefault(b => b.Id == id);
+
+                if (book == null)
+                {
+                    return new HttpNotFoundResult($"Cannot find book with ID {id}");
+                }
+
+                var userID = User.Identity.GetUserId();
+                if (book.AuthorId!=userID)
+                {
+                    return new HttpUnauthorizedResult("You cannot touch this!");
+                }
+
+
+                return View(book);
+
+            }
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Book bookViewModel)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var book = db.Books.SingleOrDefault(b => b.Id == id);
+
+                if (book==null)
+                {
+                    return HttpNotFound($"Book with ID {id} not found!");
+                }
+
+                var userID = User.Identity.GetUserId();
+                if (book.AuthorId != userID)
+                {
+                    return new HttpUnauthorizedResult("You cannot touch this!");
+                }
+
+                db.Books.Remove(book);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+        }
+    }
+}
